ad05af68feaa7a78b833caf39b456c8d7e30ac84
Giovanny Castanharo <giovanycast@hotmail.com>
2022-07-04 14:20:57 -0300

    using multer to get the body data from Post routes

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -6,13 +6,16 @@ import { SignInController } from "../modules/accounts/useCases/signIn/SignInCont
 import { ConfirmateRegisterController } from "../modules/accounts/useCases/confirmateRegister/ConfirmateRegisterController"
 import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
 import { GetProfileController } from "../modules/accounts/useCases/getProfile/GetProfileController";
-
-import { ensureAuthenticated } from "../shared/middlewares/ensureAuthenticated";
 import { RefreshTokenController } from "../modules/accounts/useCases/refreshToken/RefreshTokenController";
 import { LogOutController } from "../modules/accounts/useCases/logout/LogOutController";
 import { SendForgotPasswordMailController } from "../modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { RetrievePasswordController } from "../modules/accounts/useCases/retrivePassword/RetrievePasswordController";
 import { GeneratePdfController } from "../modules/accounts/useCases/generatePdf/GeneratePdfController";
+//middlewares
+import { ensureAuthenticated } from "../shared/middlewares/ensureAuthenticated";
+import multer from "multer"
+
+const upload = multer()
 
 const accountRoutes = Router()
 
@@ -27,24 +30,19 @@ const sendForgotPasswordMailController = new SendForgotPasswordMailController()
 const retrievePasswordController = new RetrievePasswordController()
 const generatePdfController = new GeneratePdfController()
 
-accountRoutes.post("/sign-in", createUserController.handle)
+accountRoutes.post("/sign-in", upload.none(), createUserController.handle)
 accountRoutes.get("/sign-in", signInController.handle)
 accountRoutes.post("/confirmation", confirmateRegisterController.handle)
-
 //se der problema com sendMail pode ser por causa do // baseUrl: (no tsconfig)
-
-accountRoutes.post("/log-in", authenticateUserController.handle)
-accountRoutes.post("/refresh-token", refreshTokenController.handle)
-
+accountRoutes.post("/log-in", upload.none(), authenticateUserController.handle)
+accountRoutes.post("/refresh-token", upload.none(), refreshTokenController.handle)
 accountRoutes.get("/profile", ensureAuthenticated, getProfileController.handle)
-
 accountRoutes.post("/log-out", ensureAuthenticated, logOutController.handle)
+accountRoutes.post("/forgot-password", upload.none(), sendForgotPasswordMailController.handle)
+accountRoutes.post("/retrieve-password", upload.none(), retrievePasswordController.handle)
+accountRoutes.post("/gen-pdf", ensureAuthenticated, upload.none(), generatePdfController.handle)
 
-accountRoutes.post("/forgot-password", sendForgotPasswordMailController.handle)
-accountRoutes.post("/retrieve-password", retrievePasswordController.handle)
-accountRoutes.post("/gen-pdf", ensureAuthenticated, generatePdfController.handle)
 
-// como passar o bearer token automaticamente para todas as rotas
 //como mandar do ensureAuthenticated para /refresh-token?
 
 //header 
